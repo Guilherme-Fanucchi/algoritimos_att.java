34e8bc4a4b4caec6cc31744f845ac41068a9fe7e
Guilherme-Fanucchi <164789717+Guilherme-Fanucchi@users.noreply.github.com>
2025-04-01 21:14:26 -0300

    Add files via upload

---
+++ prato_sujo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class prato_sujo {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Dgite o numero de pratos sujos: ");
+        int ps = sc.nextInt();
+        
+        while (ps > 0){
+            
+            System.out.println("Lavando pratos: " + ps);
+            ps--;
+            if (ps == 0){
+                System.out.println("Todos os pratos lavados!");
+            }
+        } 
+        }
+    }
+
